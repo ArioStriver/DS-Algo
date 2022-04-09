5abeb7b046ade42f24e215064b33134dab3338bf
Aritra Das <aritrad917@gmail.com>
2022-04-09 17:22:21 +0530

    Alternate positive and negative modified

--- Array/AlternatePositiveandNegative.java
+++ Array/AlternatePositiveandNegative.java
@@ -1,4 +1,6 @@
 /*
+(AMAZON, PAYTM, VMWARE,MICROSOFT, INTUIT)
+
 Given an unsorted array Arr of N positive and negative numbers. Your task is to create an array of alternate positive and negative numbers without changing the 
 relative order of positive and negative numbers.
 Note: Array should start with positive number.
